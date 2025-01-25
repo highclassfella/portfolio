f6c5d80e689878300c58e8f021384f25281e9f28
Anna Meszaros <anna.meszaros@gmx.de>
2025-01-25 15:40:18 +0100

    updated structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Sidebar from './components/sidebar/Sidebar'
-import Home from './components/home/Home';
-import About from './components/about/About';
-
-
-
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css';
+import Nav from './nav/Nav';
+import About from './about/About';
+import Skills from './skills/Skills';
+import Projects from './projects/Projects';
+import Blog from './blog/Blog';
+import Contact from './contact/Contact';
 
 
 const App = () => {
   return (
-    <>
-    <Sidebar />
-    <main className='main'>
-      <Home />
-      <About />
+    <Router>
+    <Nav />
+    <Routes>
+      <Route path="/" element={About}/>
+      <Route path="/skills" element={Skills}/>
+      <Route path="/projects" element={Projects}/>
+      <Route path="/blog" element={Blog}/>
+      <Route path="/contact" element={Contact}/>
+    </Routes>
 
-    </main>
-    </>
+    </Router>
   )
 }
 
