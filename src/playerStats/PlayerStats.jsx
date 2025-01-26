b8e38eb849aa26755a698e3a962369528f0ae513
Anna Meszaros <anna.meszaros@gmx.de>
2025-01-26 13:19:19 +0100

    some icons added

--- src/playerStats/PlayerStats.jsx
+++ src/playerStats/PlayerStats.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from 'react'
 import "../playerStats/playerStats.css";
+import github from "../assets/icon_github.png"
+import linkedin from "../assets/icon_linkedin.png"
+import xing from "../assets/icon_xing.png"
+import medium from "../assets/icon_medium.png"
 
 const PlayerStats = () => {
     return (
@@ -10,6 +14,12 @@ const PlayerStats = () => {
           <span id="playerStats__lines__thin"></span>
         </div>
         <h2>Fullstack Developer</h2>
+        <div id="playerStats__icons">
+          <img class="icon" src={github} width="50px" />
+          <img class="icon" src={linkedin}  />
+          <img class="icon" src={xing}  />
+          <img class="icon" src={medium}  />
+        </div>
       </div>
     );
   };
